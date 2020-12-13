5bfd75030ff768566962f3fadbc3964619232ba8
Matthew Wigmore <matthew.wigmore96@gmail.com>
2020-12-13 22:40:24 +0000

    day 12 unfinished

---
+++ 2020/day_12/solution_part_1.py
@@ -0,0 +1,4 @@
+directions = []
+with open('C:/Users/matth/Documents/advent_of_code/2020/day_12/direction_list.txt','r') as input_file:
+    for line in input_file:
+        directions.append(line.rstrip())
