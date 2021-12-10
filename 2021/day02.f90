6542e2e61cc0243399e4526e1694adea704b990d
Matthew Wigmore <matthew.wigmore96@gmail.com>
2021-12-10 12:25:54 +0000

    Solution for part 2 and updated readme

--- 2021/day02.f90
+++ 2021/day02.f90
@@ -1,6 +1,43 @@
 program day02
+    use strings
+    implicit none
+    character(50) :: fname, moveDirection, line
+    character(50), dimension(3) :: args
+    integer :: iostatus, h_p1 = 0, d_p1 = 0, h_p2 = 0, d_p2 = 0, aim = 0, moveAmount, nargs
 
-    ! solve and print part 1
-    print *, "Test day 2"
+    fname='data/day02.txt'
+    ! import input data
+    ! unit, file name & location, status (old = current file), iostat = status message (0 = fine, negative = error), action = possible actions with the file
+    open(101, file=fname, status='old', action='read', iostat=iostatus)
+    if ( iostatus /= 0 ) stop "Error opening file input file"
+
+    do ! read file and complete operations during read
+        read(101,'(A)', iostat=iostatus) line ! I'm almost certain that is is bad Fortran 90 and is left over from 77
+        if(iostatus/=0) then ! avoid end of file error
+            exit
+        else
+            call parse(line, ' ', args, nargs)
+            moveDirection = args(1) ! assign direction
+            read(args(2),*) moveAmount ! assign amount as integer
+            if (moveDirection == 'down') then
+                d_p1 = d_p1 + moveAmount
+                aim = aim + moveAmount
+            else if (moveDirection == 'up') then
+                d_p1 = d_p1 - moveAmount
+                aim = aim - moveAmount
+            else if (moveDirection == 'forward') then
+                h_p1 = h_p1 + moveAmount
+                h_p2 = h_p2 + moveAmount
+                d_p2 = d_p2 + moveAmount * aim
+            else
+                stop "Movement command not recognised"
+            end if
+        end if
+    end do
+
+    ! print part 1
+    print *, "Final position for part 1 is horizontal ", h_p1, " and depth ", d_p1, "with a h*d of ", h_p1*d_p1
+    ! print part 2
+    print *, "Final position for part 1 is horizontal ", h_p2, " and depth ", d_p2, " (aim = ", aim, ") with a h*d of ", h_p2*d_p2
 
 end program
--- 2021/readme.md
+++ 2021/readme.md
@@ -7,3 +7,7 @@ Initially I decided on something I through was typical, read in the data then pa
 In the end there is not any measurable difference in run time (both versions taking on average between 0.01s and 0.03s to run), although I suspect for a much longer list only reading through the data once would be measurably faster.  I think I will use this method for future problems when using the data during reading is possible.
 
 ### Day 2
+
+For day 2 I used the [strings module](https://gbenthien.net/strings/index.html]) by George to split the direction and move amount and once again did the coordinate update during file read-in.  The 'split' subroutine should work for this, however the output of the string after the delimiter didn't seem to work, so I used the 'parse' subroutine instead.  Probably not quite as clean but it worked.
+
+Additionally, I had trouble reading in each line of the input file with read(101,*, iostat=iostatus) line where 'line' was initialised as character(30) because it seemed to stop reading at the space between the direction and distance.  I fixed this by using read(101,'(A)', iostat=iostatus) line which I'm almost certain is bad Fortran 90 (I think it is left over from Fortran 77) but again, it worked...
